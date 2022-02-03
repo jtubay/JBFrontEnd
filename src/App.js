c16b1f86afcfc828beca509d8e7b96293b722207
Jean Tubay <jean.tubay@levvels-mbp-5.lan>
2022-02-03 15:24:54 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
 import {BrowserRouter as Router, Route ,Routes } from "react-router-dom";
-import logo from './logo.svg';
 import './App.css';
 import  Navbar  from './components/Navbar/Navbar';
 import Footer  from './components/Footer/Footer';
